5e19e4e627418eee69cfa46d53732e8f4fa90164
AnjahNyOny <110890442+AnjahNyOny@users.noreply.github.com>
2026-03-10 00:06:49 -0400

    ui

--- script.js
+++ script.js
@@ -4,15 +4,34 @@
 */
 
 function getComputerChoice() {
+    cChoice.setAttribute("style", "")
     let res = Math.floor(Math.random() * 3);
     switch (res) {
         case 0:
+            cChoice.setAttribute("style",
+                `height: 80px; width: 80px; cursor: pointer;
+                background: url('./img/rock-svgrepo-com.svg');
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;`)
             return "rock";
             break;
         case 1:
+            cChoice.setAttribute("style",
+                `height: 80px; width: 80px; cursor: pointer;
+                background: url('./img/paper-sheet-svgrepo-com.svg');
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;`)
             return "paper";
             break;
         case 2:
+            cChoice.setAttribute("style",
+                `height: 80px; width: 80px; cursor: pointer;
+                background: url('./img/scissors_1230737.svg');
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;`)
             return "scisors";
             break;
         default:
@@ -98,24 +117,36 @@ const result = document.querySelector("#roundRes");
 let humanScore = 0;
 let computerScore = 0;
 
+const hChoice = document.querySelector("#humanChoice")
+const cChoice = document.querySelector("#compChoice")
+
+
 const cScore = document.querySelector("#computerScore");
 const hScore = document.querySelector("#humanScore");
+cScore.textContent = `Score tegna : ${humanScore}`;
+hScore.textContent = `Nen'i Maitre : ${computerScore}`;
 const updateScore = () => {
-    cScore.textContent = `Your Score : ${humanScore}`;
-    hScore.textContent = `Computer Score : ${computerScore}`;
+    cScore.textContent = `Score tegna : ${humanScore}`;
+    hScore.textContent = `Nen'i Maitre : ${computerScore}`;
 }
 
 const rockBtn = document.querySelector("#rock");
 rockBtn.addEventListener("click", () => {
     let res = playRound("rock", getComputerChoice());
+    hChoice.setAttribute("style",
+    `height: 80px; width: 80px; cursor: pointer;
+    background: url('./img/rock-svgrepo-com.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;`)
     if (res === "win") {
-        result.textContent = "win";
+        result.textContent = "Nandresy an!";
         humanScore++;
     } else if (res === "lost") {
-        result.textContent = "lost";
+        result.textContent = "Hehe resy kkk!";
         computerScore++;
     } else {
-        result.textContent = "draw"
+        result.textContent = "Mba kely tsa resy oe zay!"
     }
     updateScore();
     checkWinner();
@@ -124,14 +155,20 @@ rockBtn.addEventListener("click", () => {
 const paperBtn = document.querySelector("#paper");
 paperBtn.addEventListener("click", () => {
     let res = playRound("paper", getComputerChoice());
+    hChoice.setAttribute("style",
+        `height: 80px; width: 80px; cursor: pointer;
+        background: url('./img/paper-sheet-svgrepo-com.svg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;`)
     if (res === "win") {
-        result.textContent = "win";
+        result.textContent = "Nandresy an!";
         humanScore++;
     } else if (res === "lost") {
-        result.textContent = "lost";
+        result.textContent = "Hehe resy kkk!";
         computerScore++;
     } else {
-        result.textContent = "draw"
+        result.textContent = "Mba kely tsa resy oe zay!"
     }
     updateScore();
     checkWinner();
@@ -140,14 +177,20 @@ paperBtn.addEventListener("click", () => {
 const scisorsBtn = document.querySelector("#scisors");
 scisorsBtn.addEventListener("click", () => {
     let res = playRound("scisors", getComputerChoice());
+    hChoice.setAttribute("style",
+    `height: 80px; width: 80px; cursor: pointer;
+    background: url('./img/scissors_1230737.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;`)
     if (res === "win") {
-        result.textContent = "win";
+        result.textContent = "Nandresy an!";
         humanScore++;
     } else if (res === "lost") {
-        result.textContent = "lost";
+        result.textContent = "Hehe resy kkk!";
         computerScore++;
     } else {
-        result.textContent = "draw"
+        result.textContent = "Mba kely tsa resy oe zay!"
     }
     updateScore();
     checkWinner();
@@ -171,30 +214,30 @@ const checkWinner = () => {
 
     const resetBtn = document.createElement("button");
     const main = document.querySelector("main")
-    if (humanScore===3) {
+    if (humanScore === 3) {
         disableButtons();
         finalRes.textContent = "Congrats! You won";
         main.appendChild(resetBtn);
         resetBtn.textContent = "Recommancer";
         resetBtn.addEventListener("click", () => {
-            humanScore = 0; 
-            computerScore = 0; 
-            enableButtons(); 
-            updateScore(); 
+            humanScore = 0;
+            computerScore = 0;
+            enableButtons();
+            updateScore();
             resetBtn.remove();
             finalRes.textContent = "";
             result.textContent = "";
-        });    
-    } else if (computerScore===3) {
+        });
+    } else if (computerScore === 3) {
         disableButtons();
         finalRes.textContent = "Mba nandra koa ka !!!";
         main.appendChild(resetBtn);
         resetBtn.textContent = "Recommancer";
         resetBtn.addEventListener("click", () => {
-            humanScore = 0; 
-            computerScore = 0; 
-            enableButtons(); 
-            updateScore(); 
+            humanScore = 0;
+            computerScore = 0;
+            enableButtons();
+            updateScore();
             resetBtn.remove();
             finalRes.textContent = "";
             result.textContent = "";
@@ -202,47 +245,6 @@ const checkWinner = () => {
     } else {
         finalRes.textContent = "";
     }
-    
+
 }
 
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-
-
-//     for (let i = 0; i < 5; i++) {
-//         console.log(`Round: ${i + 1}`);
-
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         let res = playRound(humanSelection, computerSelection);
-//         // console.log(`Result: ${res}`);
-
-//         switch (res) {
-//             case "win":
-//                 humanScore++;
-//                 console.log(`HumanScore: ${humanScore}`);
-//                 console.log(`ComputerScore: ${computerScore}`);
-
-//                 break;
-//             case "lost":
-//                 computerScore++;
-//                 console.log(`HumanScore: ${humanScore}`);
-//                 console.log(`ComputerScore: ${computerScore}`);
-//                 break;
-//             default:
-//                 console.log(`HumanScore: ${humanScore}`);
-//                 console.log(`ComputerScore: ${computerScore}`);
-//                 break;
-//         }
-
-//     }
-
-//     if (humanScore > computerScore) {
-//         console.log(`Final result: You win ${humanScore} - ${computerScore}`);
-//     } else if (humanScore < computerScore) {
-//         console.log(`Final result: You lose ${humanScore} - ${computerScore}`);
-//     } else {
-//         console.log(`Draw: ${humanScore} - ${computerScore}`);
-//     }
-// }
