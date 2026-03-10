d3d64d7911ea14add511ebba8c6dfe5603eb9b55
AnjahNyOny <110890442+AnjahNyOny@users.noreply.github.com>
2026-03-10 13:39:51 -0400

    ui-done

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function getComputerChoice() {
             break;
         case 1:
             cChoice.setAttribute("style",
-                `height: 80px; width: 80px; cursor: pointer;
+                `height: 80px; width: 80px;
                 background: url('./img/paper-sheet-svgrepo-com.svg');
                 background-repeat: no-repeat;
                 background-size: cover;
@@ -27,7 +27,7 @@ function getComputerChoice() {
             break;
         case 2:
             cChoice.setAttribute("style",
-                `height: 80px; width: 80px; cursor: pointer;
+                `height: 80px; width: 80px;
                 background: url('./img/scissors_1230737.svg');
                 background-repeat: no-repeat;
                 background-size: cover;
@@ -122,19 +122,33 @@ const cChoice = document.querySelector("#compChoice")
 
 
 const cScore = document.querySelector("#computerScore");
+const robotText = document.querySelector("#robotText");
 const hScore = document.querySelector("#humanScore");
-cScore.textContent = `Score tegna : ${humanScore}`;
-hScore.textContent = `Nen'i Maitre : ${computerScore}`;
+const humanText = document.querySelector("#humanText");
+humanText.textContent = `Nenareo : ${humanScore}`;
+hScore.setAttribute("style",
+    `height: 80px; width: 80px;
+    background: url('./img/face-with-rolling-eyes-svgrepo-com.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;`)
+robotText.textContent = `Robot : ${computerScore}`;
+cScore.setAttribute("style",
+    `height: 80px; width: 80px;
+        background: url('./img/robot-svgrepo-com.svg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;`)
 const updateScore = () => {
-    cScore.textContent = `Score tegna : ${humanScore}`;
-    hScore.textContent = `Nen'i Maitre : ${computerScore}`;
+    robotText.textContent = `Robot : ${computerScore}`;
+    humanText.textContent = `Nenareo : ${humanScore}`;
 }
 
 const rockBtn = document.querySelector("#rock");
 rockBtn.addEventListener("click", () => {
     let res = playRound("rock", getComputerChoice());
     hChoice.setAttribute("style",
-    `height: 80px; width: 80px; cursor: pointer;
+        `height: 80px; width: 80px; cursor: pointer;
     background: url('./img/rock-svgrepo-com.svg');
     background-repeat: no-repeat;
     background-size: cover;
@@ -178,7 +192,7 @@ const scisorsBtn = document.querySelector("#scisors");
 scisorsBtn.addEventListener("click", () => {
     let res = playRound("scisors", getComputerChoice());
     hChoice.setAttribute("style",
-    `height: 80px; width: 80px; cursor: pointer;
+        `height: 80px; width: 80px; cursor: pointer;
     background: url('./img/scissors_1230737.svg');
     background-repeat: no-repeat;
     background-size: cover;
@@ -209,6 +223,7 @@ const enableButtons = () => {
 }
 
 const finalRes = document.querySelector("#res");
+const restart = document.querySelector(".restart")
 
 const checkWinner = () => {
 
@@ -216,9 +231,15 @@ const checkWinner = () => {
     const main = document.querySelector("main")
     if (humanScore === 3) {
         disableButtons();
-        finalRes.textContent = "Congrats! You won";
-        main.appendChild(resetBtn);
-        resetBtn.textContent = "Recommancer";
+        finalRes.textContent = "Eny e, kely mba nandresy!";
+        restart.appendChild(resetBtn);
+        resetBtn.textContent = "Ao mamerena";
+        resetBtn.setAttribute("style",
+            `background: #878585;
+            cursor: pointer;
+            padding: 5px;
+            color: #ebebeb;
+            border: 1px solid #ebebeb;`)
         resetBtn.addEventListener("click", () => {
             humanScore = 0;
             computerScore = 0;
@@ -227,12 +248,20 @@ const checkWinner = () => {
             resetBtn.remove();
             finalRes.textContent = "";
             result.textContent = "";
+            hChoice.setAttribute("style", "")
+            cChoice.setAttribute("style", "")
         });
     } else if (computerScore === 3) {
         disableButtons();
         finalRes.textContent = "Mba nandra koa ka !!!";
-        main.appendChild(resetBtn);
-        resetBtn.textContent = "Recommancer";
+        restart.appendChild(resetBtn);
+        resetBtn.textContent = "Ao mamerena";
+        resetBtn.setAttribute("style",
+            `background: #878585;
+                cursor: pointer;
+            padding: 5px;
+            color: #ebebeb;
+            border: 1px solid #ebebeb;`)
         resetBtn.addEventListener("click", () => {
             humanScore = 0;
             computerScore = 0;
@@ -241,6 +270,8 @@ const checkWinner = () => {
             resetBtn.remove();
             finalRes.textContent = "";
             result.textContent = "";
+            cChoice.setAttribute("style", "")
+            hChoice.setAttribute("style", "")
         });
     } else {
         finalRes.textContent = "";
